e8f89c4858c4d54fbe28b498e017ee6ec6e511e4
shreyas <shreyu.raju@gmail.com>
2024-01-04 20:50:51 +0530

    crudapp

---
+++ backend/Server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT | 5000
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("CRUD OP");
+});
+
+app.listen(PORT, () => console.log(`Listening at ${PORT}`));
